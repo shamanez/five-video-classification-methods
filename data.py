fd04844f08a710df26d66bad7e48b8d10c0c3d6e
Matt Harvey <harveym@gmail.com>
2017-05-19 20:28:32 -0700

    Start of moving over to a data-augmentation approach.

--- data.py
+++ data.py
@@ -22,8 +22,6 @@ class DataSet():
         """
         self.seq_length = seq_length
         self.class_limit = class_limit
-        self.sequence_path = './data/sequences/'
-        self.max_frames = 300  # max number of frames a video can have for us to use it
 
         # Get the data.
         self.data = self.get_data()
@@ -50,8 +48,7 @@ class DataSet():
         than N frames. Also limit it to classes we want to use."""
         data_clean = []
         for item in self.data:
-            if int(item[3]) >= self.seq_length and int(item[3]) <= self.max_frames \
-                    and item[1] in self.classes:
+            if int(item[3]) >= self.seq_length and item[1] in self.classes:
                 data_clean.append(item)
 
         return data_clean
@@ -96,45 +93,7 @@ class DataSet():
                 test.append(item)
         return train, test
 
-    def get_all_sequences_in_memory(self, train_test, data_type, concat=False):
-        """
-        This is a mirror of our generator, but attempts to load everything into
-        memory so we can train way faster.
-        """
-        # Get the right dataset.
-        train, test = self.split_train_test()
-        data = train if train_test == 'train' else test
-
-        print("Loading %d samples into memory for %sing." % (len(data), train_test))
-
-        X, y = [], []
-        for row in data:
-
-            if data_type == 'images':
-                frames = self.get_frames_for_sample(row)
-                frames = self.rescale_list(frames, self.seq_length)
-
-                # Build the image sequence
-                sequence = self.build_image_sequence(frames)
-
-            else:
-                sequence = self.get_extracted_sequence(data_type, row)
-
-                if sequence is None:
-                    print("Can't find sequence. Did you generate them?")
-                    raise
-
-                if concat:
-                    # We want to pass the sequence back as a single array. This
-                    # is used to pass into a CNN or MLP, rather than an RNN.
-                    sequence = np.concatenate(sequence).ravel()
-
-            X.append(sequence)
-            y.append(self.get_class_one_hot(row[1]))
-
-        return np.array(X), np.array(y)
-
-    def frame_generator(self, batch_size, train_test, data_type, concat=False):
+    def frame_generator(self, batch_size, train_test):
         """Return a generator that we can use to train on. There are
         a couple different things we can return:
 
@@ -144,6 +103,9 @@ class DataSet():
         train, test = self.split_train_test()
         data = train if train_test == 'train' else test
 
+        print(data[0])
+        return
+
         print("Creating %s generator with %d samples." % (train_test, len(data)))
 
         while 1:
@@ -151,33 +113,19 @@ class DataSet():
 
             # Generate batch_size samples.
             for _ in range(batch_size):
-                # Reset to be safe.
-                sequence = None
 
                 # Get a random sample.
                 sample = random.choice(data)
 
-                # Check to see if we've already saved this sequence.
-                if data_type is "images":
-                    # Get and resample frames.
-                    frames = self.get_frames_for_sample(sample)
-                    frames = self.rescale_list(frames, self.seq_length)
+                # Get the frames for this sample.
+                frames = self.get_frames_for_sample(sample)
 
-                    # Build the image sequence
-                    sequence = self.build_image_sequence(frames)
-                else:
-                    # Get the sequence from disk.
-                    sequence = self.get_extracted_sequence(data_type, sample)
+                # Get a random start point and get the images for that group.
+                start_point = random.randint(0, len(frames) - self.seq_length)
+                frames = frames[start_point:self.seq_length]
+                sequence = self.build_image_sequence(frames)
 
-                if sequence is None:
-                    print("Can't find sequence. Did you generate them?")
-                    sys.exit()  # TODO this should raise
-
-                if concat:
-                    # We want to pass the sequence back as a single array. This
-                    # is used to pass into an MLP rather than an RNN.
-                    sequence = np.concatenate(sequence).ravel()
-
+                # Append the sequence to the batch.
                 X.append(sequence)
                 y.append(self.get_class_one_hot(sample[1]))
 
@@ -186,18 +134,6 @@ class DataSet():
     def build_image_sequence(self, frames):
         """Given a set of frames (filenames), build our sequence."""
         return [process_image(x, self.image_shape) for x in frames]
-
-    def get_extracted_sequence(self, data_type, sample):
-        """Get the saved extracted features."""
-        filename = sample[2]
-        path = self.sequence_path + filename + '-' + str(self.seq_length) + \
-            '-' + data_type + '.txt'
-        if os.path.isfile(path):
-            # Use a dataframe/read_csv for speed increase over numpy.
-            features = pd.read_csv(path, sep=" ", header=None)
-            return features.values
-        else:
-            return None
 
     @staticmethod
     def get_frames_for_sample(sample):
@@ -213,39 +149,6 @@ class DataSet():
         parts = filename.split('/')
         return parts[-1].replace('.jpg', '')
 
-    @staticmethod
-    def rescale_list(input_list, size):
-        """Given a list and a size, return a rescaled/samples list. For example,
-        if we want a list of size 5 and we have a list of size 25, return a new
-        list of size five which is every 5th element of the origina list."""
-        assert len(input_list) >= size
-
-        # Get the number to skip between iterations.
-        skip = len(input_list) // size
-
-        # Build our new output.
-        output = [input_list[i] for i in range(0, len(input_list), skip)]
-
-        # Cut off the last one if needed.
-        return output[:size]
-
-    @staticmethod
-    def print_class_from_prediction(predictions, nb_to_return=5):
-        """Given a prediction, print the top classes."""
-        # Get the prediction for each label.
-        label_predictions = {}
-        for i, label in enumerate(data.classes):
-            label_predictions[label] = predictions[i]
-
-        # Now sort them.
-        sorted_lps = sorted(
-            label_predictions.items(),
-            key=operator.itemgetter(1),
-            reverse=True
-        )
-
-        # And return the top N.
-        for i, class_prediction in enumerate(sorted_lps):
-            if i > nb_to_return - 1 or class_prediction[1] == 0.0:
-                break
-            print("%s: %.2f" % (class_prediction[0], class_prediction[1]))
+if __name__ == '__main__':
+    data = DataSet()
+    data.frame_generator(32, 'test')
--- train.py
+++ train.py
@@ -7,8 +7,7 @@ from data import DataSet
 import time
 
 def train(data_type, seq_length, model, saved_model=None,
-          concat=False, class_limit=None, image_shape=None,
-          load_to_memory=False):
+          class_limit=None, image_shape=None):
     # Set variables.
     nb_epoch = 1000000
     batch_size = 32
@@ -19,12 +18,6 @@ def train(data_type, seq_length, model, saved_model=None,
             '.{epoch:03d}-{val_loss:.3f}.hdf5',
         verbose=1,
         save_best_only=True)
-
-    # Helper: TensorBoard
-    tb = TensorBoard(log_dir='./data/logs')
-
-    # Helper: Stop when we stop learning.
-    early_stopper = EarlyStopping(patience=100000)
 
     # Helper: Save results.
     timestamp = time.time()
@@ -48,47 +41,30 @@ def train(data_type, seq_length, model, saved_model=None,
     # Multiply by 0.7 to attempt to guess how much of data.data is the train set.
     steps_per_epoch = (len(data.data) * 0.7) // batch_size
 
-    if load_to_memory:
-        # Get data.
-        X, y = data.get_all_sequences_in_memory('train', data_type, concat)
-        X_test, y_test = data.get_all_sequences_in_memory('test', data_type, concat)
-    else:
-        # Get generators.
-        generator = data.frame_generator(batch_size, 'train', data_type, concat)
-        val_generator = data.frame_generator(batch_size, 'test', data_type, concat)
+    # Get generators.
+    generator = data.frame_generator(batch_size, 'train')
+    val_generator = data.frame_generator(batch_size, 'test')
 
     # Get the model.
     rm = ResearchModels(len(data.classes), model, seq_length, saved_model)
 
-    # Fit!
-    if load_to_memory:
-        # Use standard fit.
-        rm.model.fit(
-            X,
-            y,
-            batch_size=batch_size,
-            validation_data=(X_test, y_test),
-            verbose=1,
-            callbacks=[tb, early_stopper, csv_logger],
-            epochs=nb_epoch)
-    else:
-        # Use fit generator.
-        rm.model.fit_generator(
-            generator=generator,
-            steps_per_epoch=steps_per_epoch,
-            epochs=nb_epoch,
-            verbose=1,
-            callbacks=[tb, early_stopper, csv_logger],
-            validation_data=val_generator,
-            validation_steps=10)
+    # Use fit generator.
+    rm.model.fit_generator(
+        generator=generator,
+        steps_per_epoch=steps_per_epoch,
+        epochs=nb_epoch,
+        verbose=1,
+        callbacks=[csv_logger],
+        validation_data=val_generator,
+        validation_steps=100)
 
 def main():
     """These are the main training settings. Set each before running
     this file."""
     model = 'conv_3d'  # see `models.py` for more
     saved_model = None  # None or weights file
-    class_limit = 2  # int, can be 1-101 or None
-    seq_length = 40
+    class_limit = None  # int, can be 1-101 or None
+    seq_length = 20
     load_to_memory = True  # pre-load the sequences into memory
 
     # Chose images or features and image shape based on network.
@@ -99,18 +75,10 @@ def main():
         data_type = 'image'
         image_shape = (150, 150, 3)
     else:
-        data_type = 'features'
         image_shape = None
 
-    # MLP requires flattened features.
-    if model == 'mlp':
-        concat = True
-    else:
-        concat = False
-
     train(data_type, seq_length, model, saved_model=saved_model,
-          class_limit=class_limit, concat=concat, image_shape=image_shape,
-          load_to_memory=load_to_memory)
+          class_limit=class_limit, image_shape=image_shape)
 
 if __name__ == '__main__':
     main()
